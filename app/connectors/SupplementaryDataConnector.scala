2115204112ef6c3028843eed2ce81286fed8fc8b
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2023-03-25 17:22:55 +0000

    Add SupplementaryDataConnector

---
+++ app/connectors/SupplementaryDataConnector.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.Service
+import models.{DailySummaryResponse, ListResult, SubmissionItem, SubmissionItemStatus, javaLocalDateQueryStringBindable}
+import play.api.Configuration
+import play.api.mvc.QueryStringBindable
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+
+import java.net.URL
+import java.time.LocalDate
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class SupplementaryDataConnector @Inject()(
+                                        httpClient: HttpClientV2,
+                                        configuration: Configuration
+                                      )(implicit ec: ExecutionContext) {
+
+  private val claimChildBenefitService: Service = configuration.get[Service]("microservice.services.claim-child-benefit")
+
+  def get(id: String)(implicit hc: HeaderCarrier): Future[Option[SubmissionItem]] =
+    httpClient
+      .get(url"${claimChildBenefitService.baseUrl}/claim-child-benefit/supplementary-data/$id")
+      .execute[Option[SubmissionItem]]
+
+  def list(
+            status: Option[SubmissionItemStatus] = None,
+            created: Option[LocalDate] = None,
+            limit: Option[Int] = None,
+            offset: Option[Int] = None
+          )(implicit hc: HeaderCarrier): Future[ListResult] = {
+
+    val localDateBinder: QueryStringBindable[LocalDate] = implicitly
+    val statusBinder: QueryStringBindable[SubmissionItemStatus] = implicitly
+    val intBinder: QueryStringBindable[Int] = implicitly
+
+    val params = List(
+      status.map(statusBinder.unbind("status", _)),
+      created.map(localDateBinder.unbind("created", _)),
+      limit.map(intBinder.unbind("limit", _)),
+      offset.map(intBinder.unbind("offset", _))
+    ).flatten
+
+    val query = if (params.isEmpty) "" else {
+      params.mkString("?", "&", "")
+    }
+
+    httpClient
+      .get(new URL(s"${claimChildBenefitService.baseUrl}/claim-child-benefit/supplementary-data$query"))
+      .execute[ListResult]
+  }
+
+  def dailySummaries(implicit hc: HeaderCarrier): Future[DailySummaryResponse] =
+    httpClient
+      .get(url"${claimChildBenefitService.baseUrl}/claim-child-benefit/supplementary-data/summaries")
+      .execute[DailySummaryResponse]
+}
---
+++ it/connectors/SupplementaryDataConnectorSpec.scala
@@ -0,0 +1,182 @@
+package connectors
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import models.{DailySummary, DailySummaryResponse, ListResult, Metadata, ObjectSummary, SubmissionItem, SubmissionItemStatus, SubmissionSummary}
+import org.scalatest.OptionValues
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.WireMockHelper
+
+import java.time.temporal.ChronoUnit
+import java.time.{Clock, Instant, LocalDate, ZoneOffset}
+
+class SupplementaryDataConnectorSpec
+  extends AnyFreeSpec
+    with Matchers
+    with ScalaFutures
+    with IntegrationPatience
+    with WireMockHelper
+    with OptionValues {
+
+  private lazy val app: Application =
+    GuiceApplicationBuilder()
+      .configure("microservice.services.claim-child-benefit.port" -> server.port())
+      .build()
+
+  private lazy val connector = app.injector.instanceOf[SupplementaryDataConnector]
+
+  private val clock = Clock.fixed(Instant.now(), ZoneOffset.UTC)
+
+  "get" - {
+
+    val hc = HeaderCarrier()
+    val id = "id"
+    val url = s"/claim-child-benefit/supplementary-data/$id"
+
+    val item = SubmissionItem(
+      id = id,
+      status = SubmissionItemStatus.Submitted,
+      objectSummary = ObjectSummary(
+        location = "file",
+        contentLength = 1337L,
+        contentMd5 = "hash",
+        lastModified = clock.instant().minus(2, ChronoUnit.DAYS).truncatedTo(ChronoUnit.SECONDS)
+      ),
+      failureReason = None,
+      metadata = Metadata(clock.instant().truncatedTo(ChronoUnit.MILLIS), "correlationId"),
+      created = clock.instant().truncatedTo(ChronoUnit.SECONDS),
+      lastUpdated = clock.instant().truncatedTo(ChronoUnit.SECONDS),
+      sdesCorrelationId = "sdesCorrelationId"
+    )
+
+    "must return a submission item when the server returns OK and some submission" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(ok(Json.stringify(Json.toJson(item))))
+      )
+
+      val result = connector.get(id)(hc).futureValue
+
+      result.value mustEqual item
+    }
+
+    "must return None when the server returns NOT_FOUND" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(notFound())
+      )
+
+      val result = connector.get(id)(hc).futureValue
+
+      result mustBe None
+    }
+
+    "must return a failed future when the server returns another status" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(serverError())
+      )
+
+      connector.get(id)(hc).failed.futureValue
+    }
+  }
+
+  "list" - {
+
+    val hc = HeaderCarrier()
+    val url = s"/claim-child-benefit/supplementary-data"
+    val listResult = ListResult(
+      totalCount = 2,
+      List(
+        SubmissionSummary("id1", "Submitted", None, Instant.now.truncatedTo(ChronoUnit.MILLIS)),
+        SubmissionSummary("id2", "Processed", None, Instant.now.truncatedTo(ChronoUnit.MILLIS))
+      )
+    )
+
+    "must return a list of submissions when the server returns OK and some submissions" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(ok(Json.toJson(listResult).toString))
+      )
+
+      val result = connector.list()(hc).futureValue
+
+      result mustEqual listResult
+    }
+
+    "must add filters to the request when passed" in {
+
+      server.stubFor(
+        get(urlPathMatching(url))
+          .withQueryParam("status", equalTo("completed"))
+          .withQueryParam("created", equalTo("2022-02-01"))
+          .withQueryParam("limit", equalTo("10"))
+          .withQueryParam("offset", equalTo("5"))
+          .willReturn(ok(Json.toJson(listResult).toString))
+      )
+
+      connector.list(
+        status = Some(SubmissionItemStatus.Completed),
+        created = Some(LocalDate.of(2022, 2, 1)),
+        limit = Some(10),
+        offset = Some(5)
+      )(hc).futureValue
+    }
+
+    "must return a failed future when the server returns an error" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(serverError())
+      )
+
+      connector.list()(hc).failed.futureValue
+    }
+  }
+
+  "dailySummaries" - {
+
+    val hc = HeaderCarrier()
+    val url = s"/claim-child-benefit/supplementary-data/summaries"
+
+    "must return a list of summaries when the server returns some" in {
+
+      val summaries = List(
+        DailySummary(LocalDate.of(2022, 1, 2), 1, 2, 3, 4),
+        DailySummary(LocalDate.of(2022, 1, 1), 6, 7, 8, 9)
+      )
+
+      val json = Json.obj(
+        "summaries" -> summaries
+      )
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(ok(json.toString))
+      )
+
+      val result = connector.dailySummaries(hc).futureValue
+
+      result mustEqual DailySummaryResponse(summaries)
+    }
+
+    "must return a failed future when the server returns an error" in {
+
+      server.stubFor(
+        get(urlMatching(url))
+          .willReturn(serverError())
+      )
+
+      connector.dailySummaries(hc).failed.futureValue
+    }
+  }
+}
