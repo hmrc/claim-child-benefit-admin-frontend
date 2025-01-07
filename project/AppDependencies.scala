5cd1575f8b43ee1c6477217764a48f50cbfe6c7e
emrawsy <27211889+emrawsy@users.noreply.github.com>
2025-01-07 10:01:52 +0000

    DDCNL-9771 bump dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 object AppDependencies {
 
-  private val bootstrapVersion = "9.5.0"
+  private val bootstrapVersion = "9.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"    % "11.7.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"    % "11.8.0",
     "uk.gov.hmrc"     %% "internal-auth-client-play-30"  % "3.0.0"
   )
 
